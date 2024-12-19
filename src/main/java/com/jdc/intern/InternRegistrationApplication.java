4bd425d505adf26647bd18bfb6f318b137e56bcd
Arkar Myint Myat <arkarmyintmyat71@gmail.com>
2024-12-19 22:40:09 +0630

    first commit

---
+++ src/main/java/com/jdc/intern/InternRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdc/intern/InternRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.intern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
