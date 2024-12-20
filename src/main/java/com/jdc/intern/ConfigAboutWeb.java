2a627a6c87cb7bb053f4a7301013105fe7cd5422
Arkar Myint Myat <arkarmyintmyat71@gmail.com>
2024-12-20 17:36:10 +0630

    simple navication bar

--- src/main/java/com/jdc/intern/ConfigAboutWeb.java
+++ src/main/java/com/jdc/intern/ConfigAboutWeb.java
@@ -2,6 +2,7 @@ package com.jdc.intern;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ConfigAboutWeb implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("/site/thymeleaf");
+        registry.addViewController("/").setViewName("/home");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/static/");
     }
 }
